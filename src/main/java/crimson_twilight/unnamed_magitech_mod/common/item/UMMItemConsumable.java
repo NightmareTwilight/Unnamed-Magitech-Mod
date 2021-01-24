dc541050565fbe05d7dd6058b31ef94f28fd825b
NightmareTwilight <casper.areskoug@icloud.com>
2021-01-24 15:53:58 +0100

    Added boolean if drink Added Use action

--- src/main/java/crimson_twilight/unnamed_magitech_mod/common/item/UMMItemConsumable.java
+++ src/main/java/crimson_twilight/unnamed_magitech_mod/common/item/UMMItemConsumable.java
@@ -3,14 +3,16 @@ package crimson_twilight.unnamed_magitech_mod.common.item;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.UseAction;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 public class UMMItemConsumable extends UMMBaseItem
 {
-    public boolean canConsume = true;
+    private boolean canConsume = true;
+    private boolean isDrink = false;
+
     public UMMItemConsumable(String name) {
         super(name);
     }
@@ -44,9 +46,20 @@ public class UMMItemConsumable extends UMMBaseItem
         canConsume = b;
     }
 
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return isDrink ? UseAction.DRINK : UseAction.EAT;
+    }
+
     public ItemStack consume(ItemStack stack, World world, LivingEntity entity)
     {
         stack.setCount(stack.getCount() -1);
         return stack;
+    }
+
+    public UMMItemConsumable setDrink()
+    {
+        isDrink = true;
+        return this;
     }
 }
